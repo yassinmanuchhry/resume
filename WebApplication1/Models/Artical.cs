61945a14ec24aba084f29712b37841c5b4fd0d88
Yassin manuchhry <manuchhry@gmail.com>
2022-04-26 17:13:57 +0430

    added Artical

--- WebApplication1/Models/Artical.cs
+++ WebApplication1/Models/Artical.cs
@@ -5,12 +5,19 @@ using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
-    public class Article
+    public class Artical
     {
         public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
 
+        public Artical(long id, string title, string description, string image)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+        }
     }
 }
---
+++ WebApplication1/ViewComponents/ArticalsViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewComponents
+{
+    public class ArticalsViewComponent :ViewComponent
+
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articals = new List<Artical>
+            { 
+              new Artical(id:1,title:"آموزش c#",description:"آموزش زبان برنامه نویسی",image:"blog-post-thumb-1.jpg"),
+              new Artical(id:2,title:" Asp.netCoreآموزش ",description:"آموزش زبان برنامه نویسی",image:"blog-post-thumb-2.jpg"),
+              new Artical(id:3,title:"آموزش Asp.Net Mvc",description:"آموزش زبان برنامه نویسی",image:"blog-post-thumb-3.jpg"),
+              new Artical(id:4,title:"آموزش Flutter",description:"آموزش زبان برنامه نویسی",image:"blog-post-thumb-4.jpg"),
+            };
+            return View("_Artical", articals);
+        }
+    }
+}
